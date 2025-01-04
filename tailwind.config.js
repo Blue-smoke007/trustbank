da552a2e88c99960cdfbd927a9283952e079e8a2
jvstln <173433315+jvstln@users.noreply.github.com>
2025-01-04 05:45:56 +0100

    Restructure project and setup tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./pages/**/*.{html,js}", "index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
