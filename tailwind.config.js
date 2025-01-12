a3ce78c070e95b33401dad7535a002586ed23cc6
bluesmoke <onuohamiracle159@gmail.com>
2025-01-13 00:39:54 +0100

    transfer design complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,27 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1.5s ease-out forwards',
+        slideInLeft: 'slideInLeft 1.5s ease-out forwards',
+        slideInRight: 'slideInRight 1.5s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        slideInRight: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 }
-
